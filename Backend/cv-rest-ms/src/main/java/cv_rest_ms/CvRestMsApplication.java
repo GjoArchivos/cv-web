13dd950ad2f3fbb911863a410905197ea0b85c14
GjoArchivos <122058484+GjoArchivos@users.noreply.github.com>
2026-03-03 01:20:46 -0600

    FEAT: creacion mi proyecto
    se crea el Backend y Frontend

---
+++ Backend/cv-rest-ms/src/main/java/cv_rest_ms/CvRestMsApplication.java
@@ -0,0 +1,13 @@
+package cv_rest_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvRestMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvRestMsApplication.class, args);
+	}
+
+}
---
+++ Backend/cv-rest-ms/src/test/java/cv_rest_ms/CvRestMsApplicationTests.java
@@ -0,0 +1,13 @@
+package cv_rest_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvRestMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
